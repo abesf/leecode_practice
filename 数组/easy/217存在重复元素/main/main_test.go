9ea57326c0688d9c3607f5be2770864ba90a9f77
周子童 <zhouzitong@artron.net>
2021-07-30 15:51:33 +0800

    287

--- 数组/easy/217存在重复元素/main/main_test.go
+++ 数组/easy/217存在重复元素/main/main_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestContainsDuplicate2(t *testing.T) {
-	a:=containsDuplicate2([]int{1,2,3,1})
+	a:= containsDuplicate2([]int{1,2,3,1})
 	t.Log(a)
 }
 func TestContainsDuplicate1(t *testing.T) {
-	a:=containsDuplicate1([]int{1,2,3,1})
+	a:= containsDuplicate1([]int{1,2,3,1})
 	t.Log(a)
 }
---
+++ 数组/medium/287寻找重复数medium/main/main.go
@@ -0,0 +1,46 @@
+package main
+
+func main() {
+
+}
+
+//查找数组里的重复数
+func findDuplicate(nums []int) int { return 0 }
+
+//暴力解，查找出每个元素出现次数，在查找出现次数>1的
+func findDuplicate1(nums []int) int {
+	l := len(nums)
+	collection := make(map[int]int)
+	for i := 0; i < l; i++ {
+		if _, ok := collection[nums[i]]; ok {
+			return nums[i]
+		}
+		collection[nums[i]]++
+	}
+
+	return 0
+}
+
+//todo 1二分
+func findDuplicate2(nums []int) int {
+	left,right:=0,len(nums)-1
+	l:=len(nums)
+	for left<right{
+		mid:=(left+right)>>1
+		counter:=0
+		for i:=0;i<l;i++{
+			if nums[i]<=mid{
+				counter++
+			}
+		}
+		if counter>mid{
+			right=mid
+		}else{
+			left=mid+1
+		}
+	}
+	return left
+}
+
+//todo 1快慢指针
+func findDuplicate3(nums []int) int { return 0 }
